1e59e3338d2b1b2ffaa2a426ca61bdff94157232
Sowrabh <sksseervi@gmail.com>
2026-03-30 05:16:12 +0000

    Delete: notification Task

--- sales-auth-service/app/tasks/notification_tasks.py
+++
@@ -1,7 +0,0 @@
-from app.core.celery_app import celery
-
-@celery.task(name="notification.send_signup_email")
-def send_signup_email(payload):
-    # This will NOT run here
-    # It will be consumed by notification service worker
-    pass
